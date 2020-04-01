158632c11cdcf3d163226c31d1123ff814d80be5
Michael Baudin <michael.baudin@gmail.com>
2020-04-01 15:16:58 +0200

    Used the bench class in the demo of R-S

--- otbenchmark/RminusSReliabilityBenchmarkProblem.py
+++ otbenchmark/RminusSReliabilityBenchmarkProblem.py
@@ -8,15 +8,38 @@ from otbenchmark.ReliabilityBenchmarkProblem import ReliabilityBenchmarkProblem
 import openturns as ot
 
 class RminusSReliabilityBenchmarkProblem(ReliabilityBenchmarkProblem):
-    def __init__(self, threshold = 0.0):
+    def __init__(self, threshold = 0.0, 
+                 muR = 4.0,
+                 sigmaR = 1.0,
+                 muS = 2.0,
+                 sigmaS = 1.0):
         """
         Creates a R-S reliability problem.
-        The event is {R - S < threshold}.
+        The event is {g(X) < threshold} where 
+        
+        g(R, S) = R - S
+        
+        We have R ~ Normal(muR, sigmaR) and S ~ Normal(muS, sigmaS). 
+        
+        Waarts (2000) uses muR = 7.0 and muS = 2.0 leading to 
+        beta = 3.54. 
         
         Parameters
         ----------
         threshold : float
             The threshold. 
+        
+        muR : float
+            The mean of the R gaussian distribution. 
+        
+        sigmaR : float
+            The standard deviation of the R gaussian distribution. 
+        
+        muS : float
+            The mean of the S gaussian distribution. 
+        
+        sigmaS : float
+            The standard deviation of the S gaussian distribution. 
                 
         Example
         -------
@@ -24,10 +47,10 @@ class RminusSReliabilityBenchmarkProblem(ReliabilityBenchmarkProblem):
         """
         limitStateFunction = ot.SymbolicFunction(["R", "S"],["R - S"])
         
-        R = ot.Normal(4., 1.)
+        R = ot.Normal(muR, sigmaR)
         R.setDescription("R")
         
-        S = ot.Normal(2., 1.)
+        S = ot.Normal(muS, sigmaS)
         S.setDescription("S")
         
         myDistribution = ot.ComposedDistribution([R, S])
