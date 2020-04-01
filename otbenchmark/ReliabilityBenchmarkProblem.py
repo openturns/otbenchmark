a53044bc1e821630192314f464eb9efcaf2ee389
Michael Baudin <michael.baudin@gmail.com>
2020-04-01 17:00:47 +0200

    Added comments to the ReliabilityBenchmark. Added pretty-print to the ReliabilityProblem.

--- otbenchmark/ReliabilityBenchmarkProblem.py
+++ otbenchmark/ReliabilityBenchmarkProblem.py
@@ -5,6 +5,8 @@
 Class to define a benchmark problem.
 """
 
+import openturns as ot
+
 class ReliabilityBenchmarkProblem:
     def __init__(self, name, thresholdEvent, probability, description = ""):
         """
@@ -26,10 +28,75 @@ class ReliabilityBenchmarkProblem:
         return None
 
     def getEvent(self):
+        """
+        Returns the event.
+        
+        Parameters
+        ----------
+        None.
+        
+        Returns
+        -------
+        event: ot.ThresholdEvent
+            The event.
+        """
         return self.thresholdEvent
     
     def getProbability(self):
+        """
+        Returns the probability.
+        
+        Parameters
+        ----------
+        None.
+        
+        Returns
+        -------
+        probability: float
+            The probability of the event.
+        """
         return self.probability
 
     def getName(self):
+        """
+        Returns the name of the problem.
+        
+        Parameters
+        ----------
+        None.
+        
+        Returns
+        -------
+        name: str
+            The name of the problem.
+        """
         return self.name
+    
+    def computeBeta(self):
+        """
+        Returns the beta of the reliability problem. 
+        This is the quantile of the 
+        probability of a standard gaussian distribution. 
+        
+        Parameters
+        ----------
+        None.
+        
+        Returns
+        -------
+        beta: float
+            The beta of the problem.
+        """
+        beta = ot.Normal().computeQuantile(self.probability, True)[0]
+        return beta
+    
+    def __str__(self):
+        g = self.thresholdEvent.getFunction()
+        beta = ot.Normal().computeQuantile(self.probability, True)[0]
+        threshold = self.thresholdEvent.getThreshold()
+        inputVector = self.thresholdEvent.getAntecedent()
+        distribution = inputVector.getDistribution()
+        s = "Name: %s \nFunction: %s\nProbability= %s\nThreshold= %s\nDistribution=%s" % (
+            self.name, g, self.probability, threshold, distribution)
+        return s
+    
