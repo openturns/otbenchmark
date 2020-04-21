d5f86368d0684fa871d7ca060ffeea48bd1a3ca7
Youssef Jebroun <jebronyoussef@gmail.com>
2020-04-21 18:28:43 +0200

    mise à jour fichier __init__

--- otbenchmark/__init__.py
+++ otbenchmark/__init__.py
@@ -6,17 +6,6 @@ from .IshigamiSensitivityBenchmarkProblem import IshigamiSensitivityBenchmarkPro
 from .DrawEvent import DrawEvent
 from .RminusSReliabilityBenchmarkProblem import RminusSReliabilityBenchmarkProblem
 from .ReliabilityProblem53 import ReliabilityProblem53
-from .ReliabilityProblem22 import ReliabilityProblem22
-from .ReliabilityProblem24 import ReliabilityProblem24
-from .ReliabilityProblem25 import ReliabilityProblem25
-from .ReliabilityProblem28 import ReliabilityProblem28
-from .ReliabilityProblem31 import ReliabilityProblem31
-from .ReliabilityProblem35 import ReliabilityProblem35
-from .ReliabilityProblem55 import ReliabilityProblem55
-from .ReliabilityProblem57 import ReliabilityProblem57
-from .ReliabilityProblem75 import ReliabilityProblem75
-from .ReliabilityProblem89 import ReliabilityProblem89
-from .ReliabilityProblem111 import ReliabilityProblem111
 from .FourBranchSerialSystemReliabilityBenchmarkProblem import FourBranchSerialSystemReliabilityBenchmarkProblem
 #from .CentralDispersionBenchmarkProblem import CentralDispersionBenchmarkProblem
 
@@ -28,17 +17,5 @@ __all__ = ['ReliabilityBenchmarkProblem',\
            'DrawEvent', 
            'RminusSReliabilityBenchmarkProblem', 
            'ReliabilityProblem53', 
-           'ReliabilityProblem22',
-           'ReliabilityProblem24',
-           'ReliabilityProblem25',
-           'ReliabilityProblem28',
-           'ReliabilityProblem31',
-           'ReliabilityProblem35',
-           'ReliabilityProblem55',
-           'ReliabilityProblem57',
-           'ReliabilityProblem75',
-           'ReliabilityProblem89',
-           'ReliabilityProblem111',
-           
            'FourBranchSerialSystemReliabilityBenchmarkProblem']
 __version__ = '1.0'
