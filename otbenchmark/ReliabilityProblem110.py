28a06a6ade4d34c246167737b654068c858ff39a
Youssef Jebroun <jebronyoussef@gmail.com>
2020-04-23 12:24:30 +0200

    Ajout du RP89 et RP110

---
+++ otbenchmark/ReliabilityProblem110.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 11:05:38 2020
+
+@author: Jebroun
+
+Class to define the ReliabilityProblem110 benchmark problem.
+"""
+
+from otbenchmark.ReliabilityBenchmarkProblem import ReliabilityBenchmarkProblem
+import openturns as ot
+import numpy as np
+
+class ReliabilityProblem110(ReliabilityBenchmarkProblem):
+    def __init__(self, threshold = 0.0, 
+                 mu1 = 0.0,
+                 sigma1 = 1.0,
+                 mu2 = 0.0,
+                 sigma2 = 1.0):
+        """
+        Creates a reliability problem RP110.
+
+        The event is {g(X) < threshold} where 
+        
+        g(x1, x2) = min(g1, g2) with :
+            
+            g1 = 0.85 - 0.1 * x1  if  x1 <= 3.5
+            
+            else 
+            
+            g1 = 4 - x1
+            
+            and 
+            
+            g2 = 2.3 - x2  if  x2 <= 2
+            
+            else 
+            
+            g2 = 0.5 - 0.1 * x2
+
+        We have x1 ~ Normal(mu1, sigma1) and x2 ~ Normal(mu2, sigma2). 
+        
+        Parameters
+        ----------
+        threshold : float
+            The threshold. 
+        
+        mu1 : float
+            The mean of the X1 gaussian distribution. 
+        
+        sigma1 : float
+            The standard deviation of the X1 gaussian distribution. 
+        
+        mu2 : float
+            The mean of the X2 gaussian distribution. 
+        
+        sigma2 : float
+            The standard deviation of the X2 gaussian distribution. 
+        """
+        def f(x):
+            if x[0] <= 3.5:
+                g1 = 0.85 - 0.1 * x[0]
+
+            else :
+                g1 = 4 - x[0]
+            if x[1] <= 2:
+                g2 = 2.3 - x[1]
+            else :
+                g2 = 0.5 - 0.1 * x[1]
+    
+            g = np.amin(np.stack((g1, g2)), 0)
+            y = [g]
+            
+            return y
+        
+        
+        
+        limitStateFunction = ot.PythonFunction(2, 1, f)
+
+        X1 = ot.Normal(mu1, sigma1)
+        X1.setDescription(["X1"])
+        X2 = ot.Normal(mu2, sigma2)
+        X2.setDescription(["X2"])
+
+        myDistribution = ot.ComposedDistribution([X1, X2])
+        inputRandomVector = ot.RandomVector(myDistribution)
+        outputRandomVector = ot.CompositeRandomVector(limitStateFunction, inputRandomVector)
+        thresholdEvent = ot.ThresholdEvent(outputRandomVector, ot.Less(),
+                                           threshold)
+
+        name = "RP110"
+        probability = 0.0000319
+        super(ReliabilityProblem110, self).__init__(name, thresholdEvent, probability)
+        
+        return None
+
