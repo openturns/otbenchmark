aa05fd6a9cf902ffd5fe537ac1d21ff92147f2d0
Youssef Jebroun <jebronyoussef@gmail.com>
2020-04-23 12:52:07 +0200

    Test pour RP24 RP 22

---
+++ tests/test_ReliabilityProblem24.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 10:55:16 2020
+
+@author: Jebroun
+
+Test for ReliabilityBenchmarkProblem24 class.
+"""
+import otbenchmark as otb
+import unittest
+import numpy as np
+import openturns as ot
+
+
+class CheckReliabilityProblem24(unittest.TestCase):
+
+    def test_ReliabilityBenchmarkProblem24(self):
+        problem = otb.ReliabilityProblem24()
+        print(problem)
+
+        # Check probability
+        pf = problem.getProbability()
+        pf_exacte = 0.00286
+        np.testing.assert_allclose(pf, pf_exacte, rtol=1.e-15)
+
+        # Check function
+        event = problem.getEvent()
+        function = event.getFunction()
+        X = [10.0, 10.0]
+        Y = function(X)
+        assert(type(Y) is ot.Point)
+        np.testing.assert_allclose(Y[0], 2.5)
+
+    def test_UseCase(self):
+        problem = otb.ReliabilityProblem24()
+        event = problem.getEvent()
+
+        # Create a Monte Carlo algorithm
+        experiment = ot.MonteCarloExperiment()
+        algo = ot.ProbabilitySimulationAlgorithm(event, experiment)
+        algo.setMaximumCoefficientOfVariation(0.05)
+        algo.setMaximumOuterSampling(int(1e5))
+        algo.run()
+
+        # Retrieve results
+        result = algo.getResult()
+        computed_pf = result.getProbabilityEstimate()
+        exact_pf = problem.getProbability()
+        print('exact_pf=', exact_pf)
+        print('computed_pf=', computed_pf)
+        samplesize = result.getOuterSampling() * result.getBlockSize()
+        print("Sample size : ", samplesize)
+        atol = 1.0 / np.sqrt(samplesize)
+        np.testing.assert_allclose(computed_pf, exact_pf, atol=atol)
+
+if __name__ == "__main__":
+    unittest.main()
