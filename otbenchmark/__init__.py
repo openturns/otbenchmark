169dcf9466c6e19e70b754a4d81857b3bd5619c7
Youssef Jebroun <jebronyoussef@gmail.com>
2020-04-23 12:21:49 +0200

    Mise à jour du fichier __init__.py

--- otbenchmark/__init__.py
+++ otbenchmark/__init__.py
@@ -8,6 +8,20 @@ from .IshigamiSensitivityBenchmarkProblem import IshigamiSensitivityBenchmarkPro
 from .DrawEvent import DrawEvent
 from .RminusSReliabilityBenchmarkProblem import RminusSReliabilityBenchmarkProblem
 from .ReliabilityProblem53 import ReliabilityProblem53
+from .ReliabilityProblem22 import ReliabilityProblem22
+from .ReliabilityProblem24 import ReliabilityProblem24
+from .ReliabilityProblem25 import ReliabilityProblem25
+from .ReliabilityProblem28 import ReliabilityProblem28
+from .ReliabilityProblem31 import ReliabilityProblem31
+from .ReliabilityProblem35 import ReliabilityProblem35
+from .ReliabilityProblem55 import ReliabilityProblem55
+from .ReliabilityProblem57 import ReliabilityProblem57
+from .ReliabilityProblem75 import ReliabilityProblem75
+from .ReliabilityProblem89 import ReliabilityProblem89
+from .ReliabilityProblem110 import ReliabilityProblem110
+from .ReliabilityProblem111 import ReliabilityProblem111
+from .FourBranchSerialSystemReliabilityBenchmarkProblem import FourBranchSerialSystemReliabilityBenchmarkProblem
+
 from .FourBranchSerialSystemReliabilityBenchmarkProblem import (
     FourBranchSerialSystemReliabilityBenchmarkProblem,
 )
@@ -23,6 +37,17 @@ __all__ = [
     "DrawEvent",
     "RminusSReliabilityBenchmarkProblem",
     "ReliabilityProblem53",
-    "FourBranchSerialSystemReliabilityBenchmarkProblem",
-]
+	"ReliabilityProblem22",
+    "ReliabilityProblem24",
+    "ReliabilityProblem25",
+    "ReliabilityProblem28",
+    "ReliabilityProblem31",
+    "ReliabilityProblem35",
+	"ReliabilityProblem55",
+    "ReliabilityProblem57",
+    "ReliabilityProblem75",
+    "ReliabilityProblem89",
+    "ReliabilityProblem111",
+    "ReliabilityProblem110",
+    "FourBranchSerialSystemReliabilityBenchmarkProblem"]
 __version__ = "1.0"
