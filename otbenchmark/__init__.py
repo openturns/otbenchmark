6c14fc7c4dbd58624453e31ad203a55eead30f7c
Youssef Jebroun <jebronyoussef@gmail.com>
2020-06-03 15:58:09 +0200

    list of all the problems

--- otbenchmark/__init__.py
+++ otbenchmark/__init__.py
@@ -28,6 +28,7 @@ from .ReliabilityProblem91 import ReliabilityProblem91
 from .ReliabilityProblem63 import ReliabilityProblem63
 from .ReliabilityProblem60 import ReliabilityProblem60
 from .ReliabilityProblem77 import ReliabilityProblem77
+from .mini_Benchmark import mini_Benchmark
 from .FourBranchSerialSystemReliability import FourBranchSerialSystemReliability
 
 # from .CentralDispersionBenchmarkProblem import
@@ -63,6 +64,7 @@ __all__ = [
     "ReliabilityProblem63",
     "ReliabilityProblem60",
     "ReliabilityProblem77",
+    "mini_Benchmark",
     "FourBranchSerialSystemReliability",
 ]
 __version__ = "1.0"
