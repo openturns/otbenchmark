73c30d54e2ea17ee9ce31fe05c89b423b14292b3
Michael BAUDIN <michael.baudin@gmail.com>
2021-12-04 19:59:16 +0100

    Added sampling algorithm for conditional distribution

--- tests/test_ConditionalDistribution.py
+++ tests/test_ConditionalDistribution.py
@@ -84,33 +84,42 @@ class CheckConditionalDistribution(unittest.TestCase):
         # We condition X = (X0, X1, X2) given X1=2.0, X2=3.0
         conditionalIndices = [1, 2]
         conditionalReferencePoint = [2.0, 3.0]
-        conditionalDistribution = ot.Distribution(
-            otbenchmark.ConditionalDistribution(
-                distribution, conditionalIndices, conditionalReferencePoint
+        for useIntegration in [True, False]:
+            conditionalDistribution = ot.Distribution(
+                otbenchmark.ConditionalDistribution(
+                    distribution,
+                    conditionalIndices,
+                    conditionalReferencePoint,
+                    useIntegration=useIntegration,
+                )
             )
-        )
-        # PDF
-        computed = conditionalDistribution.computePDF([1.0])
-        print("computed PDF=", computed)
-        conditionedPDF = distribution.computePDF(
-            [1.0, conditionalReferencePoint[0], conditionalReferencePoint[1]]
-        )
-        n2 = ot.Normal(2)
-        conditioningPDF = n2.computePDF(
-            [conditionalReferencePoint[0], conditionalReferencePoint[1]]
-        )
-        exact = conditionedPDF / conditioningPDF
-        np.testing.assert_allclose(computed, exact)
-        # CDF
-        computed = conditionalDistribution.computeCDF([1.0])
-        print("computed CDF=", computed)
-        exact = 0.8413447460685339
-        np.testing.assert_allclose(computed, exact)
-        # CDF when X0 = INF, X2 = INF
-        computed = conditionalDistribution.computeCDF([7.0])
-        print("computed CDF=", computed)
-        exact = 1.0
-        np.testing.assert_allclose(computed, exact)
+            # Configure tolerance depending on algorithm
+            if useIntegration:
+                rtol = 1.0e-7
+            else:
+                rtol = 1.0e-2
+            # PDF
+            computed = conditionalDistribution.computePDF([1.0])
+            print("computed PDF=", computed)
+            conditionedPDF = distribution.computePDF(
+                [1.0, conditionalReferencePoint[0], conditionalReferencePoint[1]]
+            )
+            n2 = ot.Normal(2)
+            conditioningPDF = n2.computePDF(
+                [conditionalReferencePoint[0], conditionalReferencePoint[1]]
+            )
+            exact = conditionedPDF / conditioningPDF
+            np.testing.assert_allclose(computed, exact)
+            # CDF
+            computed = conditionalDistribution.computeCDF([1.0])
+            print("computed CDF=", computed)
+            exact = 0.8413447460685339
+            np.testing.assert_allclose(computed, exact, rtol=rtol)
+            # CDF when X0 = INF, X2 = INF
+            computed = conditionalDistribution.computeCDF([7.0])
+            print("computed CDF=", computed)
+            exact = 1.0
+            np.testing.assert_allclose(computed, exact)
 
     def test_ConditionalDistribution3(self):
         # Do not condition anything.
