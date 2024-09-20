7b608a08d781a47d0ee27ecc27807042392b2124
Julien Schueller <schueller@phimeca.com>
2024-09-20 16:30:06 +0200

    Unpin matplotlib

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     keywords=["OpenTURNS", "benchmark"],
     version="0.1.1",
     packages=find_packages(),
-    install_requires=["numpy", "matplotlib", "scipy", "openturns>=1.16"],
+    install_requires=["numpy<2", "matplotlib", "openturns>=1.23"],
     description="Benchmark problems for OpenTURNS",
     long_description=long_description,
     long_description_content_type="text/markdown",
